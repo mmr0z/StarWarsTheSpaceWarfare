a9396b3a0f16c1e6fabfc9ad7ecb1ac37de6d6a7
mmr0z <maciej_mroz@outlook.com>
2022-05-31 10:42:28 +0200

    Update main.cpp

--- StarWarsSpaceWarfare/main.cpp
+++ StarWarsSpaceWarfare/main.cpp
@@ -5,23 +5,7 @@ int main() {
     // create the window
     sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
 
-    // create some shapes
-    sf::CircleShape circle(100.0);
-    circle.setPosition(100.0, 300.0);
-    circle.setFillColor(sf::Color(100, 250, 50));
-
-    sf::RectangleShape rectangle(sf::Vector2f(120.0, 60.0));
-    rectangle.setPosition(500.0, 400.0);
-    rectangle.setFillColor(sf::Color(100, 50, 250));
-
-    sf::ConvexShape triangle;
-    triangle.setPointCount(3);
-    triangle.setPoint(0, sf::Vector2f(0.0, 0.0));
-    triangle.setPoint(1, sf::Vector2f(0.0, 100.0));
-    triangle.setPoint(2, sf::Vector2f(140.0, 40.0));
-    triangle.setOutlineColor(sf::Color::Red);
-    triangle.setOutlineThickness(5);
-    triangle.setPosition(600.0, 100.0);
+    // create some objects
 
     // run the program as long as the window is open
     while (window.isOpen()) {
@@ -34,13 +18,13 @@ int main() {
                 window.close();
         }
 
+        // LOGIC
+
         // clear the window with black color
         window.clear(sf::Color::Black);
 
+        // DRAW
         // draw everything here...
-        window.draw(circle);
-        window.draw(rectangle);
-        window.draw(triangle);
 
         // end the current frame
         window.display();
